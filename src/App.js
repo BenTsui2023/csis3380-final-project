a9dc20f1ac981e6428ea8d703985dae194f8cfc7
BenTsui2023 <kei25ha9737@gmail.com>
2023-03-07 19:49:42 -0800

    Improved the code

--- src/App.js
+++ src/App.js
@@ -21,41 +21,40 @@ const Product = (props) => {
 function App() {
 
   const [mealData, setMealData] = useState([]);
-  const [mealPrice, setMealPrice] = useState([{price:10.2},{price:10.2},{price:10.2},{price:10.2},{price:10.2},{price:10.2},{price:10.2},{price:10.2}]);
-  const [mealDataWithPrice, setMealDataWithPrice] = useState([]);
+  const [mealPrice] = useState([{price:10.2},{price:10.2},{price:10.2},{price:10.2},{price:10.2},{price:10.2},{price:10.2},{price:10.2}]);
+  //const [mealDataWithPrice, setMealDataWithPrice] = useState([]);
 
   useEffect(() => {
     fetch(
       "https://api.spoonacular.com/food/menuItems/search?apiKey=6e6ba03597a84042837973e85d2f2a7d&query=burger&number=8"
-      //'www.themealdb.com/api/json/v1/1/filter.php?apiKey=1&c=Seafood'
     )
       .then((response) => response.json())
       .then((data) => {
         setMealData(data.menuItems);
-        console.log(data.menuItems)
+        //console.log(data.menuItems)
       })
       .catch(() => {
         console.log("error");
       });
   }, []);
 
-  useEffect(() => {
-    // Combine mealData and mealPrice into a new array of objects
-    const newData = mealData.map((data, index) => ({...data, ...mealPrice[index]}));
-    setMealDataWithPrice(newData);
-  }, [mealData, mealPrice]);
+  // useEffect(() => {
+  //   // Combine mealData and mealPrice into a new array of objects
+  //   const newData = mealData.map((data, index) => ({...data, ...mealPrice[index]}));
+  //   setMealDataWithPrice(newData);
+  // }, [mealData, mealPrice]);
 
   return (
     <div className = "App">
       <div className='header'><p>Online Restaurant</p></div>
       <div className='filter'><p>This is filter</p></div>
       <div className='wrapperForProductList'>
-        {mealDataWithPrice.map(menu =>
+        {mealData.map((menu , index) =>
           <Product
             key={menu.id}
             image={menu.image}
             title={menu.title}
-            price={menu.price}
+            price={mealPrice[index].price}
           />
         )}
       </div>
